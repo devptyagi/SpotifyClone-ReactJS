75f0960faf192fe01d6bd3c3fd06aeef495adaa4
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2021-04-17 08:47:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     const hash = getTokenFromUrl();
-    window.location = "";
+    window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
